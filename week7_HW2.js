32739d21e809e6ea7c20c16adb7281a38bb02d35
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-16 17:45:56 -0400

    Create week7_HW2.js

---
+++ week7_HW2.js
@@ -0,0 +1,7 @@
+// Question 5: Complete 3 Codewars problems using JavaScript, start with ones you have already solved in python.  Paste a link here to the 3 questions you completed
+
+1. https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/python
+
+2. 
+
+3. 
