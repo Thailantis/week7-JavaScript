d1d1c45e8d3f1092412479ee6b5340ec6e19f837
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-05-18 23:03:36 -0400

    Update week7_HW4.js

--- week7_HW4.js
+++ week7_HW4.js
@@ -1,31 +1,47 @@
-document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault();
+function fetchData(season, round) {
+    const url = `https://ergast.com/api/f1/${season}/${round}/driverStandings.json`;
+
+    return axios.get(url)
+    .then(response => response.data.MRData.StandingsTable.StandingsList[0].DriverStandings)
+    .catch(error => {
+        console.log('Error:', error);
+        return [];
+    });
+}
+
+function createTableRows(data) {
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    data.forEach(entry => {
+        const row = document.createElement('tr');
+
+        const positionCell = document.createElement('td');
+        positionCell.textContent = entry.positionText;
+        row.appendChild(positionCell);
+
+        const driverCell = document.createElement('td');
+        driverCell.textContent = `${entry.Driver.givenName} ${entry.Driver.familyName}`;
+        row.appendChild(driverCell);
+
+        const constructorCell = document.createElement('td');
+        constructorCell.textContent = entry.Constructors[0].name;
+        row.appendChild(constructorCell);
+
+        const pointsCell = document.createElement('td');
+        pointsCell.textContent = entry.points;
+        row.appendChild(pointsCell);
+
+        tableBody.appendChild(row)
+    });
+}
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
     const season = document.getElementById('season').value;
     const round = document.getElementById('round').value;
 
-    const apiUrl = 'https://ergast.com/api/f1/${season}/${round}/driverStandings.json';
-
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const driverStandings = data.MRData.StandingsTable.StandingsLists[0].driverStandings;
-
-        const tableBody = document.getElementById('table-body');
-        tableBody.innerHTML = '';
-
-        driverStandings.forEach(driver => {
-            const row = document.createElement('track');
-            row.innerHTML = `
-            <td>${driver.position}</td>
-            <td>${driver.Driver.givenName} ${driver.Driver.familyName}</td>
-            <td>${driver.Constructors[0].name}</td>
-            <td>${driver.points}</td>
-            `;
-            tableBody.appendChild(row);
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    fetchData(season, round)
+        .then(data => createTableRows(data));
 });
